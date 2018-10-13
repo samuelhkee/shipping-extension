ae3fbb9d3e889a7c7df152d07d241d4bbb433853
Vincent Xu <vincentxu9@gmail.com>
2018-10-13 02:21:22 -0700

    Stylized the expand section

--- popup.js
+++ popup.js
@@ -1,24 +1,24 @@
 var buttonTemp = document.getElementById('button1');
-
 var coll = document.getElementsByClassName("collapsible");
+var descrip = document.getElementsByClassName("content");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+
+coll[0].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
-      content.style.display = "none";
+        content.style.display = "none";
     } else {
-      content.style.display = "block";
+        content.style.display = "block";
     }
-  });
-}
+});
 
 buttonTemp.onclick = function(){
     var input = document.getElementById('trackingNumInput');
     var trackingNum = document.getElementById('trackingNum');
     var trackingSummary = document.getElementById('trackingSummary');
+
     
     var url = `http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=<?xml version="1.0" encoding="UTF-8" ?> <TrackRequest USERID="128CHROM2182"><TrackID ID="${input.value}"></TrackID></TrackRequest>`;
 
@@ -37,12 +37,15 @@ buttonTemp.onclick = function(){
 
             // Handle response
             summary = xmlDoc.getElementsByTagName("TrackSummary")[0];
+            coll[0].textContent = 'Tracking number: ' + input.value;
             if (summary == null) { // Invalid tracking number
-                trackingNum.textContent = input.value + " is an invalid tracking number!"
+                //trackingNum.textContent = input.value + " is an invalid tracking number!"
+                descrip[0].textContent = input.value + ' is an invalid tracking number!'
             }
             else {
-                trackingNum.textContent = 'Tracking number: ' + input.value;
-                trackingSummary.textContent = summary.textContent;
+                descrip[0].textContent = summary.textContent;
+                // trackingNum.textContent = 'Tracking number: ' + input.value;
+                // trackingSummary.textContent = summary.textContent;
             }
         }
     }
